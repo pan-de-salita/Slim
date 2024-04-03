8e287593562e115c7bed91f8c5e774bf98f54122
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-04-03 15:39:08 +0800

    chore: break down login footer into separate files

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,6 @@
+import LoginFooter from '../components/LoginFooter';
 import LoginHeader from '../components/LoginHeader';
 import LoginMain from '../components/LoginMain'
-
-import { TbBrandGithub } from "react-icons/tb";
-import { BiCodeCurly } from "react-icons/bi";
-import { SiNeovim } from "react-icons/si";
 
 const Login = () => {
   return (
@@ -12,37 +9,7 @@ const Login = () => {
         <LoginHeader />
         <LoginMain />
       </div>
-
-      {/* footer */}
-      <footer className='invisible md:visible lg:visible w-full h-[90px] self-end flex items-center'>
-        <ul className='w-full flex justify-center items-center gap-5 text-[#6f6f6f]'>
-          <li>
-            <a href="https://github.com/pan-de-salita" target='_blank' className='flex items-center gap-1 hover:text-[#1a69a6] hover:underline'>
-              <div>
-                <TbBrandGithub />
-              </div>
-              Github
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/pan-de-salita/Slim" target='_blank' className='flex items-center gap-1 hover:text-[#1a69a6] hover:underline'>
-              <div>
-                <BiCodeCurly />
-              </div>
-              Code
-            </a>
-          </li>
-          <li>
-            <a href="https://neovim.io/" target='_blank' className='flex items-center gap-1 hover:text-[#1a69a6] hover:underline' >
-              <div>
-                <SiNeovim />
-              </div>
-              Neovim
-            </a>
-          </li>
-        </ul>
-      </footer>
-
+      <LoginFooter />
     </div>
   );
 };
