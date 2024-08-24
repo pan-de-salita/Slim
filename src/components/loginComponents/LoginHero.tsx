ac2557e2adee6eab6db75452dc1a0b98f8537e23
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-08-24 23:29:35 +0800

    edit: modify hero copy

--- src/components/loginComponents/LoginHero.tsx
+++ src/components/loginComponents/LoginHero.tsx
@@ -5,17 +5,17 @@ const LoginHero = () => {
 
   return (
     <>
-      <h1 className='text-[2.688rem] md:text-[2.813rem] lg:text-[2.813rem] font-black mb-[0.625rem] text-center text-[#1d1c1d]' >
-        <span>
-          {isLogin ? 'Log in' : 'Sign up'}
-        </span>
+      <h1 className="mb-[0.625rem] text-center text-[2.688rem] font-black text-[#1d1c1d] md:text-[2.813rem] lg:text-[2.813rem]">
+        <span>{isLogin ? "Log in" : "Sign up"}</span>
         &nbsp;for Slack with
         <br />
-        <span className={`${isLogin ? 'text-[#7db643]' : 'text-[#45c0f1]'}`}>&nbsp;minimal&nbsp;</span>
+        <span className={`${isLogin ? "text-[#7db643]" : "text-[#45c0f1]"}`}>
+          &nbsp;lean&nbsp;
+        </span>
         functionality
       </h1>
-      <div className='mb-[2rem] text-[#454245]'>
-        <span className='font-bold'>Workspace:&nbsp;</span>
+      <div className="mb-[2rem] text-[#454245]">
+        <span className="font-bold">Workspace:&nbsp;</span>
         <span>http://206.189.91.54/api/v1</span>
       </div>
     </>
